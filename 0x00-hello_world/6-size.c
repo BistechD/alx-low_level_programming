e48a64b4820eef7a7463cb7347408694355caeab
BistechD <bistechcomputer@gmail.com>
2023-03-17 03:21:42 -0700

    HELLO WORLD

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %d byte(s)\n", sizeof(char));
+    printf("Size of short int: %d byte(s)\n", sizeof(short int));
+    printf("Size of int: %d byte(s)\n", sizeof(int));
+    printf("Size of long int: %d byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+    printf("Size of float: %d byte(s)\n", sizeof(float));
+    printf("Size of double: %d byte(s)\n", sizeof(double));
+    printf("Size of long double: %d byte(s)\n", sizeof(long double));
+    return 0;
+}
